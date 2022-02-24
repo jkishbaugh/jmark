5f4a4eedb455e31f4f417a31cab458d4f02e49eb
Justin Kishbaugh <jkishbaugh@journaltech.com>
2022-02-24 10:10:57 -0700

    store

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,19 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::io::Write;
+use crate::TagType::{P, H1};
 
+enum TagType {
+    H1,
+    P
+}
+
+fn get_closing_tag(_tag_to_close: TagType) -> String {
+    match _tag_to_close {
+        H1 => "</h1>\n",
+        P => "</p>\n",
+    }.to_string()
+}
 fn get_title() -> String {
     let mut the_title = String::from(env!("CARGO_PKG_NAME"));
     the_title.push_str(" (v");
@@ -19,47 +31,27 @@ fn parse_markdown_file(_filename: &str) {
 
     let file_to_parse = Path::new(_filename);
     let file = File::open(&file_to_parse).expect("[Error] Failed to open file!");
-
     let mut tokens: Vec<String> = Vec::new();
     let reader = BufReader::new(file);
     for line in reader.lines() {
-        let mut _p_tag: bool = false;
-        let mut _h_one_tag: bool = false;
+        let mut _tag_type: TagType= P;
+
         let line_contents = line.unwrap();
         let mut first_char: Vec<char> = line_contents.chars().take(1).collect();
         let mut output = String::new();
 
         match first_char.pop() {
             Some('#') => {
-                if _p_tag {
-                    _p_tag = false;
-                    output.push_str("</p>\n");
-                }
-                if _h_one_tag {
-                    _h_one_tag = false;
-                    output.push_str("</h1>\n");
-                }
-
-                _h_one_tag = true;
+                _tag_type= H1;
                 output.push_str("<h1>");
                 output.push_str(&line_contents[2..]);
             }
             _ => {
-                if !_p_tag {
-                    _p_tag = true;
-                    output.push_str("<p>");
-                }
+                output.push_str("<p>");
                 output.push_str(&line_contents);
             }
         }
-        if _p_tag {
-            _p_tag =false;
-            output.push_str("</p>\n");
-        }
-        if _h_one_tag {
-            _h_one_tag = false;
-            output.push_str("</h1>\n");
-        }
+        output.push_str(&get_closing_tag(_tag_type));
         if output != "<p></p>\n"{
             tokens.push(output);
         }
